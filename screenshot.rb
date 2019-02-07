b5357a9dae916501e525c18e172f7a0afcd03ed7
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-02-07 21:31:27 +0100

    changes to screenshot class

--- screenshot.rb
+++ screenshot.rb
@@ -3,39 +3,48 @@ require 'csv'
 require 'json'
 require 'date'
 
-def take_screenshot(url)
-  browser = Watir::Browser.new
+class Screenshot
+  def initialize(data_description, data)
+    @data_description = data_description
+    @data = data
+  end
 
-  match_data = url.match(/((http|https):\/\/www.|www.)(?<website>(\w+))..+/)
-  website_name = match_data[1]
+  def parse_or_screenshot
+    # need to get user to say whether url or csv or json
+    # if so, need to pass the path of the file
+    if @data_description == "url"
+      take_screenshot(@data)
+    elsif @data_description == "csv"
+      parse_csv(@data)
+    elsif @data_description == "json"
+      parse_json(@data)
+    end
+  end
 
-  browser.goto url
+  def take_screenshot(url)
+    browser = Watir::Browser.new
 
-  browser.screenshot.save "images/#{website_name} - #{DateTime.now.strftime("%e %b %Y %H:%M:%S%p")}.png"
-end
+    match_data = url.match(/((http|https):\/\/www.|www.)(?<website>(\w+))..+/)
+    website_name = match_data[1]
+    browser.goto url
 
+    screenshot = browser.screenshot.save "images/#{website_name} - #{DateTime.now.strftime("%e %b %Y %H:%M:%S%p")}.png"
+  end
 
+  def parse_csv
+    urls = []
+    CSV.foreach(@data) do |row|
+      urls << row[0]
+    end
+    urls
+  end
 
-filepath = 'urls.csv'
-
-def parse_csv(filepath)
-  CSV.foreach(filepath) do |row|
-    "#{row[0]}"
+  def parse_json
+    serialized_urls = File.read(@data)
+    results = JSON.parse(serialized_urls)
+    results["urls"].values
   end
 end
 
-parse_csv(filepath)
-
-filepath = 'urls.json'
-def parse_json(filepath)
-  serialized_urls = File.read(filepath)
-  results = JSON.parse(serialized_urls)
-  results
-end
-
-json_data = parse_json(filepath)
-p json_data["urls"].values
-
-def parse_text
-
-end
+# p Screenshot.new('csv', 'urls.csv').parse_csv
+# p Screenshot.new('json', 'urls.json').parse_json
