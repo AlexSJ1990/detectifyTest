1f431f09a02528beda7e7a79f531aea8034f3e25
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-02-07 17:02:52 +0100

    parsing files

--- screenshot.rb
+++ screenshot.rb
@@ -1,17 +1,41 @@
 require 'watir'
-
+require 'csv'
+require 'json'
+require 'date'
 
 def take_screenshot(url)
   browser = Watir::Browser.new
 
-  filename = DateTime.now.strftime("%d%b%Y%H%M%S")
-
-  urls = "https://www.google.com"
-
-  match_data = url.match(/(https:\/\/www.|www.)(?<website>(\w+))..+/)
+  match_data = url.match(/((http|https):\/\/www.|www.)(?<website>(\w+))..+/)
   website_name = match_data[1]
 
   browser.goto url
 
-  browser.screenshot.save "images/#{website_name}.png"
+  browser.screenshot.save "images/#{website_name} - #{DateTime.now.strftime("%e %b %Y %H:%M:%S%p")}.png"
+end
+
+
+
+filepath = 'urls.csv'
+
+def parse_csv(filepath)
+  CSV.foreach(filepath) do |row|
+    "#{row[0]}"
+  end
+end
+
+parse_csv(filepath)
+
+filepath = 'urls.json'
+def parse_json(filepath)
+  serialized_urls = File.read(filepath)
+  results = JSON.parse(serialized_urls)
+  results
+end
+
+json_data = parse_json(filepath)
+p json_data["urls"].values
+
+def parse_text
+
 end
