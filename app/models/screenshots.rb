4b7b920f37113e4613568d552de41d3221cddac9
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-02-08 16:55:45 +0100

    separated db methods and began interface

---
+++ app/models/screenshots.rb
@@ -0,0 +1,12 @@
+# class Screenshot
+#   # TODO: need to get the user to name the files they are sending e.g. "Monday Screenshots"
+#   # then when they want to retrieve, you'll have to present all their file_ref names so that they can choose
+#   # attr_reader :files
+#   def initialize(data_description, data, file_ref)
+#     @data_description = data_description
+#     @data = data
+#     @files = []
+#     @user_id = nil
+#     @file_ref = file_ref
+#   end
+# end
--- app/models/users.rb
+++ app/models/users.rb
@@ -2,5 +2,14 @@ class User
   def initialize(username, password)
     @username = username
     @password = password
+    @logged_on = false
   end
+
+  def logged_on?
+    @logged_on
+  end
+
+  # def login
+  #   @logged_on = true
+  # end
 end
