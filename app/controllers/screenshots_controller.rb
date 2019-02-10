70aea3087cf04ddc3df93dc23d2159722aeff243
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-02-10 15:32:55 +0100

    register and login working with password hashing

--- app/controllers/screenshots_controller.rb
+++ app/controllers/screenshots_controller.rb
@@ -8,6 +8,8 @@ class ScreenshotsController
   end
 
   def retrieve
+    # each time you save you want to create a new file that you can retrieve
+    # give them back the folder that they can go to to get their screenshot
   end
 
   # here we are creating a screenshot
@@ -16,6 +18,7 @@ class ScreenshotsController
     # TODO: how do we get user_id in here
     data_description = @view.screenshot_menu
     @view.print_info("please Enter 1, 2 or 3") unless ["1", "2", "3"].include?(data_description)
+    save_path = @view.ask_for_info("Please enter the relative path where you would like to store your screenshots")
     # put a path here to exit the program?
     if data_description == "1"
       answer = nil
@@ -34,7 +37,7 @@ class ScreenshotsController
       files = Task.new(data).parse_or_screenshot(data_description, 1)
       files.each { |file| file }
       if files.length > 1
-      "#{files} are the files"
+      p "#{files} are the files"
         files.each do |file|
           save(file, 1)
         end
@@ -42,4 +45,3 @@ class ScreenshotsController
     end
   end
 end
-
