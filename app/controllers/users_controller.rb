ca553b20775184bb82fc49bfcbacf5198db384db
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-02-10 16:12:55 +0100

    routing for login and register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,31 +10,34 @@ class UsersController
   end
 
   def register
-    username = @view.get_info("what is your username?")
-    password = @view.get_info("What is your password?")
+    username = @view.get_info("       what is your username?")
+    password = @view.get_info("       What is your password?")
     user = User.new(username: username, password: password)
     user.encrypt
     my_password = BCrypt::Password.new(user.password)
     my_password == password
     save_user(user)
+    @view.print_info("        Great, you are registered! \n")
+    @view.print_info("        Please now login")
+    login
   end
 
   def login
     username = @view.get_info("what is your username?")
     password = @view.get_info("What is your password?")
     user = find_user(username).flatten
-    user = User.new(username: user[1], password: user[2])
-    my_password = BCrypt::Password.new(user.password)
-    if user && (my_password == password)
-      @view.print_info("Welcome #{user.username.capitalize}")
-      user.login
-      return user
+    @user = User.new(username: user[1], password: user[2])
+    my_password = BCrypt::Password.new(@user.password)
+    if @user && (my_password == password)
+      @view.print_info("        Welcome #{@user.username.capitalize}")
+      @user.login
+      return @user
     else
-      @view.print_info("Sorry invalid details")
+      @view.print_info("        Sorry invalid details")
       login
     end
   end
 end
 
-UsersController.new.register
-UsersController.new.login
+# UsersController.new.register
+# UsersController.new.login
